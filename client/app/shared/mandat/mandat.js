27af756e1cd60e6d0e1e8fded4c3e697df6f5d0b
cheikhbethio <mmoussasow@gmail.com>
2016-01-30 18:44:27 +0100

    deuxieme version

--- client/app/shared/mandat/mandat.js
+++ client/app/shared/mandat/mandat.js
@@ -30,70 +30,86 @@ angular.module('shared.mandat', ['ui.router'])
 }])
 .controller('mandatController', ['$scope', '$state','$stateParams',"$localStorage", 'Mandat', 
  function($scope, $state, $stateParams,$localStorage, Mandat){  
-
  	$scope.mandats = Mandat.query();
 	$scope.success = $stateParams.success;
 	$scope.editSuccess = $stateParams.editSuccess;
+		$scope.enableSelecter = false;
+		$scope.deleteNow = false;
+		$scope.toDelete = {};
 
-	$scope.closeAlert = function() {
-        $scope.success = false;
-        $scope.editSuccess = false;
-    };
 
-	$scope.deleteMandat = function(mandatIndex) {
-		console.log(mandatIndex);
-
-		console.log("Mandat: " + JSON.stringify($scope.mandats[mandatIndex]));
-
-			Mandat.remove({id: $scope.mandats[mandatIndex]._id }, function(removed_mandat){
-				$scope.mandats.splice(mandatIndex, 1);
-			});
-    };
+	$scope.deleter = function() {
+		$scope.enableSelecter = !$scope.enableSelecter;
+		$scope.deleteNow = true;
+		if ($scope.deleteNow) {			
+			for (var m in $scope.toDelete) {
+				Mandat.remove({id: $scope.mandats[m]._id }, function(removed_mandat){
+					$scope.mandats.splice(m, 1);
+				});
+			};
+			$scope.toDelete = {};
+		} else{
+			alert('chosir les mandats à supprimer!');
+		}
+	
+    }
 
 }])
-.controller('deleteMandatDialogController', function($scope, $modalInstance, data){
-
-		$scope.mandat_title= mandat_title;
-		console.log("Title to delete: " + $scope.mandat_title);
-		
-		$scope.cancel = function(){
-			$modalInstance.dismiss('Canceled');
-		};
-		
-		$scope.save = function(){
-			$modalInstance.close();
-		};
-
-})
 .controller('createMandatController', ['$scope', '$state','$stateParams',"$localStorage", 'Mandat',
- function($scope, $state, $stateParams,$localStorage, Mandat){   
- 	$scope.addMandat = function() {
-		
-		var newMandat={
-			title : $scope.title,
-			adresse : $scope.adresse,
-			photo : $scope.photo,
-	        prix: $scope.prix,
-		    date_creation : Date.now(),
-		    date_validite : $scope.date_creation_fin
-		};
-
-		Mandat.save(newMandat);
-		$state.go('app.mandat');
+ function($scope, $state, $stateParams,$localStorage, Mandat){  	
+	$scope.compareLesDates = true;	
+	var verifDate = function(date){
+			if (Date.now() > date || date == "Invalid Date") {
+				$scope.compareLesDates = false;
+			} else{
+				$scope.compareLesDates = true;
+			}
+			return $scope.compareLesDates;
 	}
+
+ 	$scope.addMandat = function(isValidForm) {
+		var thisDate = verifDate(new Date ($scope.date_creation_fin));
+		if (isValidForm && thisDate) {
+			var newMandat={
+				title : $scope.title,
+				adresse : $scope.adresse,
+				photo : $scope.photo,
+		        prix: $scope.prix,
+			    date_creation : Date.now(),
+			    date_validite : new Date ($scope.date_creation_fin)
+			};
+			Mandat.save(newMandat);
+			$state.go('app.mandat');
+		}else{
+			alert("Le formulaire est invalide!!! \nVeillez respecter les contrainte");
+		}
+	}
+
 }])
 .controller('editMandatController', ['$scope', '$state','$stateParams',"$localStorage", 'Mandat',
  function($scope, $state, $stateParams,$localStorage, Mandat){   
-	$scope.mandat = Mandat.get({id: $stateParams.id}, function(page) {
-        console.log("get mandat "+$stateParams.id);
-    });
 
-    $scope.editMandat = function(){
-    	if ($scope.editMandatForm.$valid){ 
+	$scope.mandat = Mandat.get({id: $stateParams.id}, function(page) {
+       // console.log("get mandat "+$stateParams.id);
+    });
+	
+	$scope.compareLesDates = true;	
+	var verifDate = function(date){
+			if (Date.now() > date || date == "Invalid Date") {
+				$scope.compareLesDates = false;
+			} else{
+				$scope.compareLesDates = true;
+			}
+			return $scope.compareLesDates;
+	}
+
+    $scope.editMandat = function(isValidForm){
+		var thisDate = verifDate(new Date ($scope.mandat.date_validite));
+    	if (isValidForm && thisDate){ 
 	    	Mandat.update({id: $stateParams.id}, $scope.mandat);
 	    	$state.go('app.mandat', {editSuccess:true});
 	   	} else {
-			console.log('Formulaire Invalide.');
+			alert('Formulaire Invalide.');
 		}
     }
 }])
